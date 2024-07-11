122041e2d5697f56077a04092c77caaa36d97694
root <root@TasnimSomo>
2024-07-11 21:43:51 +0400

    0x00. MySQL advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- creates a table users
 -- id, name, email, country
-
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'IN') NOT NULL DEFAULT 'US'
+	country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL
 	)
