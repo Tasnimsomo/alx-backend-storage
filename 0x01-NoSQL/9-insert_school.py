780a5ab2983121d2e468313fbec0368e4f877c96
root <root@TasnimSomo>
2024-07-16 10:12:26 +0400

    0x01. NoSQL

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,8 +3,9 @@
 Python function that inserts a new document in a collection based on kwargs:
     """
 
+import pymongo
+
 
 def insert_school(mongo_collection, **kwargs):
-    result = mongo_collection.insert_one(kwargs)
-
-    return result.inserted_id
+    """inserts a new document in a collection"""
+    return mongo_collection.insert(kwargs)
